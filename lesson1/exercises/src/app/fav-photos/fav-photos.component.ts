f9ff7bcc95e19c6919a6ce800e21836fa7ad50f2
Emma Howard <emmachoward@gmail.com>
2021-06-22 13:34:51 -0500

    adjusted existing components

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Some Cool Images';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images.neopets.com/games/tradingcards/premium/lg_0706.gif';
+  image3 = 'https://images.neopets.com/halloween/haunted_fairie/2011/popups/character.png';
 
   constructor() { }
 
